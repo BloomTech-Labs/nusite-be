48f3cbf997ad3a03a333e4ecc15ef9b493a18672
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-14 13:35:03 -0400

    Feat: Initial Type definitions to cover RC1
    Co-authored-by: danielle baxter <danibaxx@hotmail.com>

---
+++ types/index.ts
@@ -0,0 +1,54 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+    projects: [Project!]!
+    user(id: ID!): User
+    project(id: ID!): Project
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    first_name: String!
+    last_name: String!
+    password: String!
+    email: String!
+    company: String
+    dev_experience: Int
+    dev_education: Int
+    projects: [Project!]!
+  }
+
+  type Project {
+    id: ID!
+    project_name: String!
+    project_avatar: String
+    project_description: String!
+    project_owner: User!
+    project_developer: User
+    completed: Boolean!
+    marketplace: Boolean!
+    showcase: Boolean!
+  }
+
+  type AuthPayload {
+    token: String
+    user: User
+  }
+
+  type Mutation {
+    signup(username: String!, first_name: String!, last_name: String!, password: String!, email: String!): AuthPayload
+    login(email: String!, password: String!): AuthPayload
+    # adding a project
+    # claiming a project/job
+    # update a project
+    # update a user
+    # potentially "delete" for developers
+    # archive users
+    # delete projects
+  }
+`
+
+export default typeDefs;
