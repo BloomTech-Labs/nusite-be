5083581330ccc8cec0169d351d8ea1060c33d08b
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-14 00:38:46 -0400

    Feat: base model class created for db interaction

---
+++ models/Model.ts
@@ -0,0 +1,46 @@
+import db from '../data/dbconfig';
+
+class Model {
+  tablename: string;
+  constructor(tablename: string) {
+    this.tablename = tablename;
+  }
+
+  find() {
+    return db(this.tablename)
+  }
+
+  findBy(filter: any) {
+    return db(this.tablename)
+      .where(filter)
+      .first();
+  }
+
+  findById(id: number) {
+    return db(this.tablename)
+      .where({ id })
+      .first()
+  }
+
+  add(item: any) {
+    return db(this.tablename)
+      .insert(item)
+      .returning('*')
+  }
+
+  update(id: number, item: any) {
+    return db(this.tablename)
+      .where({ id })
+      .update(item)
+      .returning('*')
+  }
+
+  remove(id: number) {
+    return db(this.tablename)
+      .where({ id })
+      .del()
+  }
+}
+
+export const User = new Model('users')
+export const Project = new Model('projects')
