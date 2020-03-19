de94c55db497a52c0280ef285ea058325b8e8a16
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-18 21:01:44 -0400

    Feat: generateToken Component for signup and login
    Co-authored-by: danielle baxter <danibaxx@hotmail.com>

---
+++ token/generateToken.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { sign } from "jsonwebtoken";
+
+const generateToken = (user: UserValues): string => {
+  const payload: PayloadValue = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const secret: string = process.env.JWT_SECRET || "super-secret-token-secret";
+
+  const options: { expiresIn: string } = {
+    expiresIn: "1d",
+  };
+
+  return sign(payload, secret, options);
+};
+
+interface PayloadValue {
+  subject: number;
+  username: string;
+}
+
+interface UserValues {
+  id: number;
+  username: string;
+}
