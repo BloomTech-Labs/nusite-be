6a0b94f166a3d860f0ba63c13ff38a04e7a125e6
Danielle Baxter <danibaxx@hotmail.com>
2020-03-18 20:30:19 -0600

    # Description Project/User Query to find users and projects and also to find User and Project by ID.
    Fixes # (issue)
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] New feature (non-breaking change which adds functionality)
    
    ## Change Status
    
    - [ ] Incomplete/work-in-progress, PR is for discussion/feedback
    
    # Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] My code has been reviewed by at least one peer
    - [ ] My changes generate no new warnings
    - [ ] There are no merge conflicts

---
+++ resolvers/Query.ts
@@ -0,0 +1,24 @@
+import { User, Project } from '../models/Model';
+
+function users() {
+  return User.find()
+}
+
+function projects() {
+  return Project.find()
+}
+
+function user(parent: any, args: { id: number }) {
+  return User.findById(args.id)
+}
+
+function project(parent: any, args: { id: number }) {
+  return Project.findById(args.id)
+}
+
+export default {
+  users,
+  projects,
+  user,
+  project,
+}
