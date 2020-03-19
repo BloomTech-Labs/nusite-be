6bc30f919b8338036702c5d1f2ddf614b6046d65
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-19 16:59:45 -0400

    Test: Initial test passing, added base test route to server

--- api/server.spec.ts
+++ api/server.spec.ts
@@ -1,10 +1,10 @@
-//const request = require('supertest');
-//const server = require('./server');
+import supertest from 'supertest'
+import server from './server'
 
 describe("root path test", () => {
   it("should check the root path of the server", async () => {
-    const res = await request(server).get("/");
+    const res = await supertest(server).get("/");
     //Does return an expected status code
     expect(res.status).toBe(200);
   });
-});
+}); 
