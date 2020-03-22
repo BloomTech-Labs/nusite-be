fb6fcdf668a1c83af357f9343e3d3beb36ce9be3
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-22 19:09:51 -0400

    Fix: Moved base route to proper location

--- api/server.ts
+++ api/server.ts
@@ -4,15 +4,16 @@ import typeDefs from "../types";
 import resolvers from "../resolvers";
 
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+server.applyMiddleware({ app, cors: false });
+
 app.use("/", (req, res) => {
   res.send("Checking Tests");
 });
-
-server.applyMiddleware({ app, cors: false });
 
 export default app;
