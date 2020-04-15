693b6e4fb3eb1f8a3e28f27ca1759656f557de98
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-14 20:26:15 -0400

    Test: added test for update

--- resolvers/__tests__/mutation.spec.ts
+++ resolvers/__tests__/mutation.spec.ts
@@ -27,4 +27,22 @@ describe("testing auth mutations", () => {
     expect(res.token).toBeDefined();
     expect(res.user).toBeDefined();
   });
+
+  test("should test update mutation", async () => {
+    const user = {
+      id: 1,
+      username: "test",
+      first_name: "test",
+      last_name: "test",
+      password: "test",
+      email: "test@testing.com",
+      company: "",
+      dev_experience: 1,
+      dev_education: 2,
+    };
+
+    const res = await Mutation.updateUser({ id: 1 }, user);
+
+    console.log(res);
+  });
 });
