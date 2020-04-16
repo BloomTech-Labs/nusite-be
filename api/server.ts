da7ffdc111f7bb534db2d39bb3255a2d84dd9973
Danielle Baxter <danibaxx@hotmail.com>
2020-04-15 19:24:42 -0600

    feat: added in auth routes and views to server.ts

--- api/server.ts
+++ api/server.ts
@@ -4,6 +4,8 @@ import passport from "passport";
 import typeDefs from "../types";
 import resolvers from "../resolvers";
 
+const authRoutes = require("../routes/auth-routes");
+
 const app = express();
 
 const server = new ApolloServer({
@@ -15,10 +17,21 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, cors: { origin: "*", credentials: true } });
 
+// set view engine - Take Out!
+app.set("view engine", "ejs");
+
 app.use(passport.initialize());
 
 app.use("/", (_req, res) => {
   res.send("Welcome to Partnerd API");
+});
+
+// auth route setup - take out?
+app.use("/auth", authRoutes);
+
+// render home - take out
+app.use("/", (_req, res) => {
+  res.render("home");
 });
 
 app.get(
@@ -35,6 +48,7 @@ app.get(
   }),
   (req, res) => {
     res.redirect("/dashboard");
-});
+  }
+);
 
 export default app;
