86d3b43d7a41f086a86b66caed9263bed6dcd97e
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-28 20:52:11 -0400

    feat: facebook route returning user and token

--- api/server.ts
+++ api/server.ts
@@ -6,7 +6,9 @@ import typeDefs from "../types";
 import resolvers from "../resolvers";
 
 import "../authconfig/linkedin";
+import "../authconfig/facebook";
 const linkedIn = require("../routes/linkedin");
+const facebook = require("../routes/facebook");
 
 const app = express();
 
@@ -25,6 +27,7 @@ const server = new ApolloServer({
 server.applyMiddleware({ app, cors: { origin: "*", credentials: true } });
 
 app.use("/", linkedIn);
+app.use("/", facebook);
 
 app.use("/", (_req, res) => {
   res.send("Welcome to Partnerd API");
--- routes/facebook.ts
+++ routes/facebook.ts
@@ -1,28 +1,28 @@
-const linkedinRouter = require("express").Router();
+const googleRouter = require("express").Router();
 import passport from "passport";
 import generateToken from "../token/generateToken";
 
-linkedinRouter.get("/auth/linkedin", passport.authenticate("linkedin"));
+googleRouter.get("/auth/facebook", passport.authenticate("facebook"));
 
 // Initial passport route calls this route
 // controls creation of token for the frontend
 // Frontend will need to redirect themselves once they save the token and user
 // will need thoroghly checked with the frontend
-linkedinRouter.get(
-  "/auth/linkedin/callback",
-  passport.authenticate("linkedin", {
+googleRouter.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
     passReqToCallback: true,
     failureRedirect: "/login",
     session: false,
   }),
-  (req: { user: { id: number; username: string } }, res: any) => {
+  (req: { user: { id: number; displayName: string } }, res: any) => {
     let user = {
       id: req.user.id,
-      username: req.user.username,
+      username: req.user.displayName,
     };
     const token = generateToken(user);
     res.status(200).json({ token, user });
   }
 );
 
-module.exports = linkedinRouter;
+module.exports = googleRouter;
