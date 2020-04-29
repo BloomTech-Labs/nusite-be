f9abdfd9ae325d21252bdad1c31ab9bc5553d350
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-28 21:36:34 -0400

    feat: dummy login working, just not saving user

--- authconfig/facebook.ts
+++ authconfig/facebook.ts
@@ -1,14 +1,14 @@
 import passport from "passport";
-import { User } from "../models/Model";
-import { hash } from "bcryptjs";
+// import { User } from "../models/Model";
+// import { hash } from "bcryptjs";
 const FacebookStrategy = require("passport-facebook");
 
 passport.use(
   new FacebookStrategy(
     {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3000/auth/facebook/callback",
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "http://localhost:4000/auth/facebook/callback",
     },
     async (
       _accessToken: string,
@@ -16,30 +16,14 @@ passport.use(
       profile: any,
       done: any
     ) => {
-      // asynchronous verification, for effect...
-      // To keep the example simple, the user's LinkedIn profile is returned to
-      // represent the logged-in user. In a typical application, you would want
-      // to associate the LinkedIn account with a user record in your database,
-      // and return that user instead.
-      const findUser = await User.findBy({ email: profile.emails[0].value });
-
-      if (!findUser) {
-        const pw = await hash(profile.displayName, 12);
-
-        const newUser = {
-          username: profile.displayName,
-          first_name: profile.name.givenName,
-          last_name: profile.name.familyName,
-          email: profile.emails[0].value,
-          password: pw,
-        };
-
-        const [{ password, ...user }]: any = await User.add(newUser);
-
-        return done(null, user);
-      } else {
-        return done(null, findUser);
-      }
+      // asynchronous function
+      // should search db for existing user and login if exists
+      // else create a user in the db and return the created user
+      console.log(profile.id);
+      console.log(profile.displayName);
+      console.log(profile.name.givenName);
+      console.log(profile.name.familyName);
+      return done(null, profile);
     }
   )
 );
