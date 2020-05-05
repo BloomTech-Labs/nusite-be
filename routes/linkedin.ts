2414f394b0c92f9d009574781fbcb1a4b510780b
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-04 20:09:01 -0400

    fix: added api to the links

--- routes/linkedin.ts
+++ routes/linkedin.ts
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const passport = require("passport");
 const { sign } = require("jsonwebtoken");
 
-router.get("/auth/linkedin", passport.authenticate("linkedin"));
+router.get("/api/auth/linkedin", passport.authenticate("linkedin"));
 
 // Initial passport route calls this route
 // controls creation of token for the frontend
 // Frontend will need to redirect themselves once they save the token and user
 // will need thoroghly checked with the frontend
 router.get(
-  "/auth/linkedin/callback",
+  "/api/auth/linkedin/callback",
   passport.authenticate("linkedin", {
     passReqToCallback: true,
     failureRedirect: "/login",
