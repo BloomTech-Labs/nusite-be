87126fc1100f1d4d418c2cc140340c9a9c72983f
Node Code Formatter <formatter@1337z.ninja>
2020-05-11 01:21:06 +0000

    Formatted code

--- authconfig/github.ts
+++ authconfig/github.ts
@@ -14,36 +14,38 @@ import { hash } from "bcryptjs";
 const GithubStrategy = require("passport-github2").Strategy;
 
 passport.use(
-  new GithubStrategy({
-    clientID: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK_URL,
-    
-  }, (_accessToken: string, _refreshToken: string, profile: any, done: any) => {
-    process.nextTick(function () {
-      console.log("GH PROFILE", profile)
-      done(null, {});
-    //   const findUser = User.findBy({
-    //     email: profile.emails[0].valuve
-    //   });
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    (_accessToken: string, _refreshToken: string, profile: any, done: any) => {
+      process.nextTick(function() {
+        console.log("GH PROFILE", profile);
+        done(null, {});
+        //   const findUser = User.findBy({
+        //     email: profile.emails[0].valuve
+        //   });
 
-    //   if (!findUser) {
-    //     const pw = hash(profile.displayName, 12);
+        //   if (!findUser) {
+        //     const pw = hash(profile.displayName, 12);
 
-    //     const newUser = {
-    //       username: profile.displayName,
-    //       first_name: profile.name.givenName,
-    //       last_name: profile.name.familyName,
-    //       email: profile.emails[0].value,
-    //       password: pw,
-    //     };
+        //     const newUser = {
+        //       username: profile.displayName,
+        //       first_name: profile.name.givenName,
+        //       last_name: profile.name.familyName,
+        //       email: profile.emails[0].value,
+        //       password: pw,
+        //     };
 
-    //     const [{ password, ...user}]: any = User.add(newUser);
+        //     const [{ password, ...user}]: any = User.add(newUser);
 
-    //     return done(null, user)
-    //   } else {
-    //     return done(null, findUser);
-    //   }
-    });
-  })
+        //     return done(null, user)
+        //   } else {
+        //     return done(null, findUser);
+        //   }
+      });
+    }
+  )
 );
--- routes/github.ts
+++ routes/github.ts
@@ -2,19 +2,21 @@ import passport from "passport";
 import generateToken from "../token/generateToken";
 const ghRouter = require("express").Router();
 
-ghRouter.get('/api/auth/github', 
+ghRouter.get(
+  "/api/auth/github",
   passport.authenticate("github", {
-    scope: ["user:email"]
+    scope: ["user:email"],
   })
 );
 
-ghRouter.get("/api/auth/github/callback", 
+ghRouter.get(
+  "/api/auth/github/callback",
   passport.authenticate("github", function(req, res) {
     console.log(req.user);
     // passReqToCallback: true,
     // failureRedirect: "/login",
     // session: false,
-  }),
+  })
   // (req: {user: {id: number; username: string } }, res: any) => {
   //   let user = {
   //     id: req.user.id,
